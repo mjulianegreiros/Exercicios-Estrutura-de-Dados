c944eda60d55ec52cbab6df99a44b1b81a18ce6f
Júlia Negreiros <93203124+mjulianegreiros@users.noreply.github.com>
2025-04-25 18:30:39 -0300

    Update livro.c

--- Lista 2/Q2/livro.c
+++ Lista 2/Q2/livro.c
@@ -1,24 +1,33 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "livro.h"
 
-typedef struct livro{
-    char titulo[50];
-    char autor[50];
+struct livro{
+    char *titulo;
+    char *autor;
 };
 
-Livro *criarLivro(char *ti , char *autor){
+Livro *criarLivro(char *titulo , char *autor){
     int i;
     Livro *l = (Livro *)malloc(sizeof(Livro));
     
-    while(ti[i] != "\n"){
-        *l->titulo(ti+i) = ti[i];
+    if(l!=NULL){
+        l->titulo = titulo;
+        l->autor = autor;
     }
-
 }
-void consultarTitulo(Livro l);
-void consultarAutor(Livro l);
-void modificarTitulo(Livro l);
-void modificarAutor(Livro l);
-void liberarLivro(Livro l);
-
-
+char *consultarTitulo(Livro *l){
+    return l->titulo;
+}
+char *consultarAutor(Livro *l){
+    return l->autor;
+}
+void modificarTitulo(Livro *l , char *novo){
+    l->titulo = novo;
+}
+void modificarAutor(Livro *l , char *novo){
+    l->autor = novo;
+}
+void liberarLivro(Livro *l){
+    free(l);
+}
