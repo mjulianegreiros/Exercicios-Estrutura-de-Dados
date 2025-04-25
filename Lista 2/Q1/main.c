5c6924da9af5f9c3c3d730b184ccf08c7c5772b1
Júlia Negreiros <93203124+mjulianegreiros@users.noreply.github.com>
2025-04-25 18:32:14 -0300

    Update main.c

--- Lista 2/Q1/main.c
+++ Lista 2/Q1/main.c
@@ -6,6 +6,12 @@ int main(){
     float largura = 5.0;
     float altura = 2.0;
     Retangulo *r1 = criarRetangulo(largura , altura);
+    printf("Area: %f" , calcularArea(r1));
+    printf("Perimetro: %f" , calcularPerimetro(r1));
+    printf("Largura: %f" , obterLargura(r1));
+    printf("Altura: %f" , obterAltura(r1));
+    liberarRetangulo(r1);
     
+
 
 }
