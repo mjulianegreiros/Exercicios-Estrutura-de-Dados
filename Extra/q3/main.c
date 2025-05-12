409825847eca4b0f3d23d23136f9a5b7646258d4
Júlia Negreiros <93203124+mjulianegreiros@users.noreply.github.com>
2025-05-12 11:50:24 -0300

    palindroma recursiva

--- Extra/q3/main.c
+++ Extra/q3/main.c
@@ -2,39 +2,60 @@
 #include <string.h>
 // saber se uma palavra é palíndroma usando recursividade
 
-int ehPalidromaIterativa(char p[]){
-    int i=0;
-    char *pRev; 
-    *pRev = inverterPalavra(p);
-    if(*p==*pRev){
-        return 1;
-    }
-
-    return 0;
-
-}
-
-char inverterPalavra(char p[]){
-    char pRev[strlen(p)];
+void inverterPalavra(char p[] , char pReversa[]){
     int i = 0;
-    int j = strlen(p) - 1;
+    int j = strlen(p);
     while(p[i]!='\0'){
-        pRev[j] = p[i];
+        pReversa[j] = p[i];
         //printf("\ni: %d  j:%d" , i  , j);
         i++;
         j--;
     }
-
-    return pRev;
+    /*i=0;
+    printf("%d" , strlen(pReversa));
+    while(pReversa[i]!='\0'){
+        printf("%c" , pReversa[i]);
+        i++;
+    }*/
+    
 }    
 
-/*int ehPalindromaRecursiva(char p[]){
-    
-}*/
+int ehPalidromaIterativa(char p[]){
+    int i=0 , eh = 0;
+    char pRev[strlen(p)-1]; 
+    inverterPalavra(p , pRev);
+    while(p[i]==pRev[i]){
+        eh = 1;
+        i++;
+    }
+
+    if(!eh){
+        return 0;
+    } return 1;
+
+}
+
+
+
+int ehPalindromaRecursiva(char p[] , int letra){
+    char pRev[strlen(p)];
+    int i=letra;
+    inverterPalavra(pRev , p);
+    if(strlen(p)==0 || strlen(p)==1){
+        return 1;
+    }else{
+        if(p[i]!=pRev[i]){
+            return 0;
+        }else{
+            i++;
+            return ehPalindromaRecursiva(p , i);
+        }
+    }
+}
 
 int main(){
-    printf("%d\n" , ehPalidromaIterativa("ana"));
-    //printf("%d\n" , ehPalidromaRecursiva("ana"));
+    char palavra[] = "banana";
+    printf("%d\n" , ehPalindromaRecursiva(palavra , 0));
     
     return 0;
 }
