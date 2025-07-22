d6804141c36c41184ee37d843eb512648865e64b
mjulianegreiros <xuxuuzinhaa8@gmail.com>
2025-07-22 14:06:28 -0300

    a

---
+++ Lista 9/main.c
@@ -0,0 +1,17 @@
+// tentativa - implementação merge sort
+#include <stdio.h>
+#include <stdlib.h>
+#define TAM 8
+
+void mergeSort(int *vet , int inicio , int fim){
+    int i , j , aux , pivo;
+    pivo = (inicio + fim)/2;
+
+    
+    
+}
+
+int main(){
+
+    return 0;
+}
